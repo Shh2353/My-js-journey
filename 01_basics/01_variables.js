5b00b7f45b2c418e41418b847c491065c770186c
Somiya Kumari <109845490+Shh2353@users.noreply.github.com>
2023-11-05 14:00:21 +0000

    lessons are added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId=144553
+let accountEmail="somiyakumari@gmail.com"
+var accountPassword="12345"
+accountPassword="12346"
+accountCity="Jaipur"
+//accountId=2 not allowed constant variable
+console.log(accountId);
+console.table([accountId,accountPassword,accountEmail])
+// Prefer not to use var coz of scope problem 
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict";//treat all js code as newer version
+//alert(3+3) we are using node.js not browser
+let name="hitesh"//string
+let age=18//number=> 2 to power 53
+let isLoggedIn=false//boolean
+//null => standalone value
+//symbol => unique
+//object
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null);//object
