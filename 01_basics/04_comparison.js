e1b373b80e2c974277ec9a81fd8e45d00228a4d7
Somiya Kumari <109845490+Shh2353@users.noreply.github.com>
2023-11-05 14:31:49 +0000

    changes are done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,9 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1);
+console.log("2"==2)//will treat 2 as a number
+console.log("2"===2)//stricly check including data type
+
