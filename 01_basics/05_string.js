0155161f4b4c05aeb6f8aa2b510cb9dc1affc9a8
Somiya Kumari <109845490+Shh2353@users.noreply.github.com>
2023-11-09 17:36:41 +0000

    updated

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,7 +1,7 @@
 const name = "hitesh"
 const repoCount = 50
 
-// console.log(name + repoCount + " Value");
+//console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
@@ -9,13 +9,10 @@ const gameName = new String('hitesh-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
-
-
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
 console.log(gameName.charAt(2));
 console.log(gameName.indexOf('t'));
-
 const newString = gameName.substring(0, 4)
 console.log(newString);
 
---
+++ 02_basics/01_array.js
@@ -0,0 +1,39 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)// insert the value from the beginning
+// myArr.shift()delete the value from the beginning
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log( newArr);
+
+
+// slice, splice
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);//element from index 1 to 3 will deleted (1 and 3 are included)
+console.log(myn2);
